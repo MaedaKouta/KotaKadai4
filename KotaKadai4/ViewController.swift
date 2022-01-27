37537e0fd0ac11684535a2769718770160f0d242
MaedaKouta <maedakoutaa@gmail.com>
2022-01-28 00:33:10 +0900

    提出用のコミット

--- KotaKadai4/ViewController.swift
+++ KotaKadai4/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var numberTextLabel: UILabel!
+    private var number: Int!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        number = 0
+        numberTextLabel.text = String(number)
     }
 
+    @IBAction func didTapCountUpButton(_ sender: Any) {
+        number += 1
+        numberTextLabel.text = String(number)
+    }
+
+    @IBAction func didTapClearButton(_ sender: Any) {
+        number = 0
+        numberTextLabel.text = String(number)
+    }
 
 }
-
