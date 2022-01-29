d3bd5043d0a71d8aa5489810e2ada6bf60b5a37e
akio0911 <akio0911@gmail.com>
2022-01-29 11:55:13 +0900

    feedback

--- KotaKadai4/ViewController.swift
+++ KotaKadai4/ViewController.swift
@@ -10,22 +10,24 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet private weak var numberTextLabel: UILabel!
-    private var number: Int!
+    private var number: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        number = 0
-        numberTextLabel.text = String(number)
+        updateNumberTextLabel()
     }
 
     @IBAction private func didTapCountUpButton(_ sender: Any) {
         number += 1
-        numberTextLabel.text = String(number)
+        updateNumberTextLabel()
     }
 
     @IBAction private func didTapClearButton(_ sender: Any) {
         number = 0
-        numberTextLabel.text = String(number)
+        updateNumberTextLabel()
     }
 
+    private func updateNumberTextLabel() {
+        numberTextLabel.text = String(number)
+    }
 }
